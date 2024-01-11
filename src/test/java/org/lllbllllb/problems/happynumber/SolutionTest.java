9b15cc5f20b4f6a608cd79ee1e7ad831c2d079ff
lll <enakes@gmail.com>
2024-01-11 23:42:14 +0100

    38

---
+++ src/test/java/org/lllbllllb/problems/happynumber/SolutionTest.java
@@ -0,0 +1,24 @@
+package org.lllbllllb.problems.happynumber;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @Test
+    void isHappy1() {
+        var res = solution.isHappy(19);
+
+        assertTrue(res);
+    }
+
+    @Test
+    void isHappy2() {
+        var res = solution.isHappy(2);
+
+        assertFalse(res);
+    }
+}
